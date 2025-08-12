0af99a00d5f889ce1436368ead1b2ef2bf3574e7
hwanginseop <his001015@gmail.com>
2025-08-12 23:27:09 +0900

    refact(BackGround.cs)
    무한 스크롤 뷰 로직 개선

--- Assets/Scrips/BackGround/BackGround.cs
+++ Assets/Scrips/BackGround/BackGround.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BackGround : MonoBehaviour
@@ -9,26 +7,23 @@ public class BackGround : MonoBehaviour
     public int endIndex;
     public Transform[] sprites;
 
-    float viewWide;
+    float spriteWidth;
+
     private void Awake()
     {
-        viewWide = Camera.main.orthographicSize * Camera.main.aspect;
+        spriteWidth = sprites[0].GetComponent<SpriteRenderer>().bounds.size.x;
     }
+
     void Update()
     {
-        Vector3 curPos = transform.position;
-        Vector3 nextPos = Vector3.left * speed * Time.deltaTime;
-        transform.position = curPos + nextPos;
+        transform.position += Vector3.left * speed * Time.deltaTime;
 
-        if (sprites[startIndex].position.x < viewWide*(-1))
+        if (sprites[startIndex].position.x < -spriteWidth)
         {
-            Vector3 backSpritePos = sprites[startIndex].localPosition;
-            Vector3 frontSpritePos = sprites[endIndex].localPosition;
-            sprites[startIndex].transform.localPosition = frontSpritePos + Vector3.right* viewWide;
-
-            int startIndexSave = startIndex;
-            startIndex = endIndex-1;
-            endIndex = (startIndexSave-1 == -1)? sprites.Length-1:startIndexSave-1;
+            Vector3 frontPos = sprites[endIndex].position;
+            sprites[startIndex].position = frontPos + Vector3.right * spriteWidth;
+            endIndex = startIndex;
+            startIndex = (startIndex + 1) % sprites.Length;
         }
     }
 }
