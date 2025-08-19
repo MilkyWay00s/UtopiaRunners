1f81e38884765bacc688b127d1de0b4b9f44facb
RedPanda <jdkyu44@naver.com>
2025-08-19 21:05:57 +0900

    DroneLauncher

---
+++ Assets/Scripts/Weapon/DroneSkill.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSkill : MonoBehaviour
+{
+    public WeaponAutoAttack weaponAutoAttack;
+    public float duration = 5f;
+    public float speedMultiplier = 2f;
+
+    private void Start()
+    {
+        if (weaponAutoAttack != null)
+        {
+            StartCoroutine(ApplySkill());
+        }
+    }
+
+    private IEnumerator ApplySkill()
+    {
+        float originalSpeed = weaponAutoAttack.weaponData.attackSpeed;
+
+        weaponAutoAttack.weaponData.attackSpeed /= speedMultiplier;
+        yield return new WaitForSeconds(duration);
+        weaponAutoAttack.weaponData.attackSpeed = originalSpeed;
+
+        Destroy(gameObject);
+    }
+}
