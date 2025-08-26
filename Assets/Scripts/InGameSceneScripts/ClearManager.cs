4a4ca12eea81f33caff4c87113bd46e94dbb5e14
thcjfd <thcjfd@naver.com>
2025-08-26 20:06:07 +0900

    ClearPanel

---
+++ Assets/Scripts/InGameSceneScripts/ClearManager.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class ClearManager : MonoBehaviour
+{
+    //클리어 판정 관련 로직
+}
---
+++ Assets/Scripts/InGameSceneScripts/InGameUIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameUIManager : MonoBehaviour
+{
+    public GameObject clearPanel;
+
+    public void onClickClear()
+    {
+        Time.timeScale = 0f;
+        clearPanel.SetActive(true);
+    }
+
+    public void OnClickRetry()
+    {
+        Time.timeScale = 1f;
+        string currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    public void OnClickNext()
+    {
+        Time.timeScale = 1f;                                   
+        string currentWorld = GameManager.Instance.currentWorld;
+        SceneManager.LoadScene(currentWorld);
+    }
+}
