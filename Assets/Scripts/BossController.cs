e67e02012f03c808cd65bd9bea0d9a7768f0d02e
thcjfd <thcjfd@naver.com>
2025-09-03 23:35:54 +0900

    PrintRandomObstacleSystem

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public ObstaclePattern[] patterns;
+    public Transform spawnOrigin;
+    public float interval = 3f;
+
+    private void Start()
+    {
+        StartCoroutine(PatternRoutine());
+    }
+
+    private IEnumerator PatternRoutine()
+    {
+        while (true)
+        {
+            ExecuteRandomPattern();
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    public void ExecutePattern(int index)
+    {
+        if (patterns.Length == 0 || index < 0 || index >= patterns.Length)
+        {
+            Debug.LogWarning("패턴이 없거나 인덱스가 잘못됨!");
+            return;
+        }
+
+        ObstaclePattern pattern = patterns[index];
+
+        foreach (var point in pattern.spawnPoints)
+        {
+            Vector2 spawnPos = (Vector2)spawnOrigin.position + point.offset;
+            Instantiate(pattern.obstaclePrefab, spawnPos, Quaternion.identity);
+        }
+    }
+
+    public void ExecuteRandomPattern()
+    {
+        int randIndex = Random.Range(0, patterns.Length);
+        ExecutePattern(randIndex);
+    }
+}
---
+++ Assets/Scripts/ObstaclePattern.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObstacleSpawnPoint
+{
+    public Vector2 offset;
+}
+
+[CreateAssetMenu(fileName = "ObstaclePattern", menuName = "ObstaclePattern")]
+public class ObstaclePattern : ScriptableObject
+{
+    public string patternName;
+    public GameObject obstaclePrefab;
+    public ObstacleSpawnPoint[] spawnPoints;
+
+    public void SpawnPattern(Vector2 bossPos)
+    {
+        foreach (var point in spawnPoints)
+        {
+            Vector2 spawnPos = bossPos + point.offset;
+            GameObject.Instantiate(obstaclePrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
+
