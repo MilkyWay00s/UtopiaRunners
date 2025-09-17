a614dd74979bbbefd286e0e6ede3ce10bee946f0
thcjfd <thcjfd@naver.com>
2025-09-17 23:04:12 +0900

    Sound Manager + Audio Mixer

--- Assets/Scripts/TitleSceneScripts/OptionsManager.cs
+++ Assets/Scripts/TitleSceneScripts/OptionsManager.cs
@@ -6,49 +6,63 @@ public class OptionsManager : MonoBehaviour
 {
     public GameObject OptionsPanel;
     public AudioMixer audioMixer;
+
     public Slider MasterVolumeSlider;
     public Slider SFXVolumeSlider;
     public Slider BGMVolumeSlider;
 
+    private const float MinSliderValue = 0.0001f; //Math.Log(0)은 음수 무한대 -> 0으로 초기화 방지
+
     void Start()
     {
-        float masterVol, bgmVol, sfxVol;
+        float masterValue = PlayerPrefs.GetFloat("MasterVolume", 1f);
+        float bgmValue = PlayerPrefs.GetFloat("BGMVolume", 1f);
+        float sfxValue = PlayerPrefs.GetFloat("SFXVolume", 1f);
 
-        audioMixer.GetFloat("MasterVolume", out masterVol);
-        audioMixer.GetFloat("BGMVolume", out bgmVol);
-        audioMixer.GetFloat("SFXVolume", out sfxVol);
+        MasterVolumeSlider.value = masterValue;
+        BGMVolumeSlider.value = bgmValue;
+        SFXVolumeSlider.value = sfxValue;
 
-        MasterVolumeSlider.value = Mathf.Pow(10, masterVol / 20f);
-        BGMVolumeSlider.value = Mathf.Pow(10, bgmVol / 20f);
-        SFXVolumeSlider.value = Mathf.Pow(10, sfxVol / 20f);
+        ApplyVolumeToMixer("MasterVolume", masterValue);
+        ApplyVolumeToMixer("BGMVolume", bgmValue);
+        ApplyVolumeToMixer("SFXVolume", sfxValue);
+
+        SetMasterVolume(masterValue);
+        SetBGMVolume(bgmValue);
+        SetSFXVolume(sfxValue);
 
         MasterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
         BGMVolumeSlider.onValueChanged.AddListener(SetBGMVolume);
         SFXVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
-    public void OpenOptions()
-    {
-        OptionsPanel.SetActive(true);
-    }
-
-    public void CloseOptions()
-    {
-        OptionsPanel.SetActive(false);
-    }
+    public void OpenOptions() => OptionsPanel.SetActive(true);
+    public void CloseOptions() => OptionsPanel.SetActive(false);
 
     public void SetMasterVolume(float value)
     {
-        audioMixer.SetFloat("MasterVolume", Mathf.Log10(value) * 20f);
+        float v = Mathf.Max(value, MinSliderValue);
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(v) * 20f);
+        PlayerPrefs.SetFloat("MasterVolume", value); 
     }
 
     public void SetBGMVolume(float value)
     {
-        audioMixer.SetFloat("BGMVolume", Mathf.Log10(value) * 20f);
+        float v = Mathf.Max(value, MinSliderValue);
+        audioMixer.SetFloat("BGMVolume", Mathf.Log10(v) * 20f);
+        PlayerPrefs.SetFloat("BGMVolume", value); 
     }
 
     public void SetSFXVolume(float value)
     {
-        audioMixer.SetFloat("SFXVolume", Mathf.Log10(value) * 20f);
+        float v = Mathf.Max(value, MinSliderValue);
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(v) * 20f);
+        PlayerPrefs.SetFloat("SFXVolume", value); 
+    }
+
+    private void ApplyVolumeToMixer(string paramName, float sliderValue)
+    {
+        float v = Mathf.Max(sliderValue, MinSliderValue);
+        audioMixer.SetFloat(paramName, Mathf.Log10(v) * 20f);
     }
 }
