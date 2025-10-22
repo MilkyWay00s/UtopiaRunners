23be3ba14df2ee8606f386fc648acc22a6a128df
hwanginseop <his001015@gmail.com>
2025-10-22 17:22:38 +0900

    feat(CharacterSelectManager.cs)
    패널 스왑 기능 구현

--- Assets/Scripts/UI/CharacterSelectScripts/CharacterSelectManager.cs
+++ Assets/Scripts/UI/CharacterSelectScripts/CharacterSelectManager.cs
@@ -12,21 +12,50 @@ public class CharacterSelectManager : SingletonObject<CharacterSelectManager>
     [SerializeField] private CharacterSpec subCharacter;
     //select scene에서 캐릭터 선택 시 저장 될 슬롯
 
-    [SerializeField] private Image selectedCharacterDesplay;
+    private Image selectedCharacterDesplay;
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject subPanel;
 
     private void Start()
     {
+        //패널 초기화
+        selectedCharacterDesplay = mainPanel.transform.Find("CharacterImage").GetComponent<Image>();
+
+        //슬롯 초기화
         selectedCharacter = null;
         mainCharacter = null;
         subCharacter = null;
     }
+    
+    public void SwapPanelSibling()
+    {
+        int m_idx = mainPanel.transform.GetSiblingIndex();
+        int s_idx = subPanel.transform.GetSiblingIndex();
+        mainPanel.transform.SetSiblingIndex(s_idx);
+        subPanel.transform.SetSiblingIndex(m_idx);
+    }
+
+    #region 버튼 이벤트
     public void OnUpgradeButtonClicked()
     {
-        SceneManager.LoadScene("5_Upgrade"); 
+        SceneManager.LoadScene("5_Upgrade");
     }
     public void CharacterSelect(CharacterSpec ch)
     {
         selectedCharacter = ch;
         selectedCharacterDesplay.sprite = selectedCharacter.displayImage;
     }
+    public void TagSelect()
+    {
+        SwapPanelSibling();
+        if (mainPanel.transform.GetSiblingIndex() < subPanel.transform.GetSiblingIndex())
+        {
+            selectedCharacterDesplay = subPanel.transform.Find("CharacterImage").GetComponent<Image>();
+        }
+        else
+        {
+            selectedCharacterDesplay = mainPanel.transform.Find("CharacterImage").GetComponent<Image>();
+        }
+    }
+    #endregion
 }
