57ab309b5a5ac196c2e0142bd91576ff7628be5e
thcjfd <thcjfd@naver.com>
2025-12-06 21:50:34 +0900

    3rd change

---
+++ Assets/Scripts/GameSystem/CharacterSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSceneManager : MonoBehaviour
+{
+    public void onWeaponButtonClicked()
+    {
+        SceneManager.LoadScene("6_WeaponSelectMode");
+    }
+
+    public void onGoButtonClicked()
+    {
+        SceneManager.LoadScene("7_InGameScene");
+    }
+
+    public void onStageButtonClicked()
+    {
+        SceneManager.LoadScene("2_Eden");
+    }
+}
