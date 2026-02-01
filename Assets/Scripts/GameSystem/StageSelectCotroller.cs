c4dfaf68b285ae6d425ddbfddb53d3b54d1e0327
hwanginseop <his001015@gmail.com>
2026-02-01 18:12:15 +0900

    [Update] Apply Bgm

--- Assets/Scripts/GameSystem/StageSelectCotroller.cs
+++ Assets/Scripts/GameSystem/StageSelectCotroller.cs
@@ -128,6 +128,7 @@ public class StageSelectCotroller : MonoBehaviour
         if (goCharacterSelectBeforeRunning)
         {
             SoundManager.Instance.PlaySFX(SFX.SFX2_Enter);
+            SoundManager.Instance.PlayBgm(BGM.BGM2_Select, true);
             SceneManager.LoadScene(characterSelectSceneName);
         }
         else
--- Assets/Scripts/InGameSystem/InGameManager.cs
+++ Assets/Scripts/InGameSystem/InGameManager.cs
@@ -26,6 +26,7 @@ public class InGameManager : MonoBehaviour
 
         // CharacterManager 찾고 이벤트 구독
         cm = FindObjectOfType<CharacterManager>();
+        SoundManager.Instance.PlayBgm(BGM.BGM3_Dangerous,true);
         /*
         if (cm != null)
         {
@@ -79,6 +80,7 @@ public class InGameManager : MonoBehaviour
     public void onStageSelectButtonClicked()
     {
         SoundManager.Instance.PlaySFX(SFX.SFX1_Click);
+        SoundManager.Instance.PlayBgm(BGM.BGM2_Select, true);
         SceneManager.LoadScene("2_Eden");
     }
 
